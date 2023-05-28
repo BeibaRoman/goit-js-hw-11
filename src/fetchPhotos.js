6b1e946dc07009625f3527e8526671b8003a6caf
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-05-28 17:18:33 +0300

    add 11 homework

---
+++ src/fetchPhotos.js
@@ -0,0 +1,49 @@
+const axios = require('axios').default;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default class ApiPhotoService {
+  constructor() {
+    this.URL = 'https://pixabay.com/api/';
+    this.KEY = '36833134-be534b8a8a704feb8c4d9a967';
+    this.PAGE = 1;
+    this.PER_PAGE = 40;
+    this.searchQuery = '';
+  }
+  async fetchPhoto() {
+    const searchParams = new URLSearchParams({
+      key: this.KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: this.PER_PAGE,
+      page: this.PAGE,
+    });
+    const newParams = searchParams.toString();
+    try {
+      const response = await axios.get(`${this.URL}?${newParams}`);
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      this.PAGE += 1;
+      return response.data;
+    } catch (error) {
+      Notify.failure(error.message);
+    }
+  }
+  get page() {
+    return this.PAGE;
+  }
+  set page(newPage) {
+    this.PAGE = newPage;
+  }
+  resetPage() {
+    this.PAGE = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
